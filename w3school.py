41582565e8ee39aa68794526153c4a15448e8eb5
justtdream <alina.dauzhanova@gmail.com>
2025-01-22 11:35:21 +0500

    edit file

--- w3school.py
+++ w3school.py
@@ -1,3 +1,4 @@
+#booleans
 a = 200
 b = 33
 
@@ -5,3 +6,29 @@ if b>a:
     print ("b is greater than a")
 else:
     print ("b is not greater than a")
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+def myfun():
+    return True
+if myfun ():
+    print ("YES")
+else:
+    print ("NO")
+
+#operators
+print (10+5)
+print (10-2)
+print(3*2)
+print(6/2)
+print (7%2)  #остаток
+print(2**5)
+print(7//2)  #целое число при делении
+
+#list
+
+    
